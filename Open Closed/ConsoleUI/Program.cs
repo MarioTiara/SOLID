8c3023e5751891dcc109ae177e5123a260ce7ffa
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-07 23:15:07 +0700

    Add Program class

--- Open Closed/ConsoleUI/Program.cs
+++ Open Closed/ConsoleUI/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OCPLibrary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<PersonModel> applicants= new List<PersonModel>{
+            new PersonModel{FirstName="Tim", LastName="Corey"},
+            new PersonModel{FirstName="Sue",LastName="Storm"},
+            new PersonModel {FirstName="nancy", LastName="Roman"}
+        };
+
+        List<EmployeModel> employes = new List<EmployeModel>();
+        Accounts accountsProcessor = new Accounts();
+
+        foreach ( var person in applicants){
+            employes.Add(accountsProcessor.Create(person));
+        }
+
+        foreach (var emp in employes){
+            Console.WriteLine($"{emp.FirstName} {emp.LastName}: {emp.EmailAddress}");
+        }
+    }
+}
