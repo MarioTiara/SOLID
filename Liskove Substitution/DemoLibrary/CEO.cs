b49df74e040b31e44df21e419b5f290abe33c9a6
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-09 21:52:01 +0700

    [LSP] add DemoLibrary

---
+++ Liskove Substitution/DemoLibrary/CEO.cs
@@ -0,0 +1,26 @@
+using System.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class CEO:Employee
+    {
+        public override void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 150M;
+            Salary = baseAmount*rank;
+        }
+
+        public override void AssignManager(Employee manager)
+        {
+            throw new InvalidOperationException("The CEO has no manager");
+        }
+
+        public void GeneratePerformanceReview(){
+            Console.WriteLine("I'm reviewing a direct report's performance.");
+        }
+    }
+}
---
+++ Liskove Substitution/DemoLibrary/Employee.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }    
+        public Employee Manager { get; set; }
+        public decimal Salary { get; set; } 
+
+        public virtual void AssignManager(Employee manager) {
+            Manager= manager;
+        }
+
+        public virtual void CalculatePerHourRate(int rank){
+            decimal baseAmount=12.50M;
+            Salary= baseAmount+(rank*2);
+        }
+    }
+}
