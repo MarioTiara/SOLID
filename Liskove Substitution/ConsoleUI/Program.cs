902d048fa632f81f020726c58a9a7e1265991dc5
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-09 21:52:23 +0700

    Add console UI

---
+++ Liskove Substitution/ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using DemoLibrary;
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Manager accountingVP= new Manager();
+            accountingVP.FirstName="mario";
+            accountingVP.LastName="pratama";
+            accountingVP.CalculatePerHourRate(4);
+
+            Employee emp=  new Manager();
+
+            emp.FirstName= "Badu";
+            emp.LastName="putra";
+            emp.AssignManager(accountingVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{emp.FirstName}'s salary is ${emp.Salary}/hour.");
+            Console.ReadLine();
+        }
+    }
+}
