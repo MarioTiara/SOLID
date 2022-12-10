49d2138a149e05dcaaa643fe049196106c5a594b
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-10 14:43:41 +0700

    [LSP] Modify CEO class

--- Liskove Substitution/DemoLibrary/CEO.cs
+++ Liskove Substitution/DemoLibrary/CEO.cs
@@ -6,21 +6,19 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class CEO:Employee
+    public class CEO:BaseEmployee, IManager
     {
         public override void CalculatePerHourRate(int rank)
         {
             decimal baseAmount = 150M;
             Salary = baseAmount*rank;
         }
-
-        public override void AssignManager(Employee manager)
-        {
-            throw new InvalidOperationException("The CEO has no manager");
-        }
-
         public void GeneratePerformanceReview(){
             Console.WriteLine("I'm reviewing a direct report's performance.");
+        }
+
+        public void FireSomeone(){
+            Console.WriteLine("You're Fired");
         }
     }
 }
