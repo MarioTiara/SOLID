b718128897c9771e0b5f93f90b6c13e70c9293cf
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-10 14:41:08 +0700

    [LSP] Add IManaged interface

---
+++ Liskove Substitution/DemoLibrary/IManaged.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public interface IManaged:IEmployee
+    {
+        IEmployee Manager {get; set;}
+        void AssignManager(IEmployee manager);
+    }
+}
