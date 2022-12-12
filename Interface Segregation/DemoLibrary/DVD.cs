5bde1e9c99f7afabc98ab5e6920d11e14285256d
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-12 20:54:00 +0700

    [ISP] Add Actors and RuntimeInMinutes in DVD class

--- Interface Segregation/DemoLibrary/DVD.cs
+++ Interface Segregation/DemoLibrary/DVD.cs
@@ -14,6 +14,8 @@ namespace DemoLibrary
         public string LibraryId { get ; set ; }
         public int Pages { get ; set ; }=-1;
         public string Title { get ; set ; }
+        public List<string> Actors { get; set; }
+        public int RuntimeInMinutes { get; set; }
 
         public void CheckIn()
         {
