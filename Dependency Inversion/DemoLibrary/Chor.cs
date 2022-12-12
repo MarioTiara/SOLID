cdf1f5e65ed5c58959b9a3915f18927e6f35c1ac
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-12 14:14:41 +0700

    [DIP] Add Constructor Injection

--- Dependency Inversion/DemoLibrary/Chor.cs
+++ Dependency Inversion/DemoLibrary/Chor.cs
@@ -5,27 +5,33 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class Chor
-    {
-        public string ChoreName { get; set; }
-        public Person Owner { get; set; }
-        public double HoursWorked { get; private set; }
-        public bool IsComplete {get;private set;}
 
-        public void Performance (double hours){
-            HoursWorked+=hours;
-            Logger log=new Logger();
-            log.Log($"Performed work on {ChoreName}");
+    public class Chor : IChor
+    {
+        private readonly ILogger _logger;
+        private readonly IMessageSender _messagesender;
+        public string ChoreName { get; set; }
+        public IPerson Owner { get; set; }
+        public double HoursWorked { get; private set; }
+        public bool IsComplete { get; private set; }
+
+        public Chor(ILogger logger, IMessageSender messageSender)
+        {
+            _logger = logger;
+            _messagesender = messageSender;
+        }
+        public void Performance(double hours)
+        {
+            HoursWorked += hours;
+            _logger.Log($"Performed work on {ChoreName}");
         }
 
-        public void CompleteChor(){
-            IsComplete=true;
+        public void CompleteChor()
+        {
+            IsComplete = true;
 
-            Logger log = new Logger();
-            log.Log($"Completed {ChoreName}");
-
-            Emailer emailer = new Emailer();
-            emailer.SendEmail(Owner, $"The Chore {ChoreName} is complete");
+            _logger.Log($"Completed {ChoreName}");
+            _messagesender.SendEmail(Owner, $"The Chore {ChoreName} is complete");
         }
     }
 }
