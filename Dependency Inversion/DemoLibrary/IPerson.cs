752c88cda101e725524083b08b8edbd8d8ebda70
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-12 14:12:36 +0700

    [DIP] Add IPerson

---
+++ Dependency Inversion/DemoLibrary/IPerson.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public interface IPerson
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string PhoneNumber { get; set; }
+        string EmailAddress { get; set; }
+    }
+}
